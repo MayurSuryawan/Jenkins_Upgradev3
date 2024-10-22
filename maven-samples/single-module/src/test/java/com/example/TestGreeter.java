dcb8b11d1fa55ee1352f84ef31bc80c11eaba748
Mayur Suryawanshi <152708653+MayurSuryawan@users.noreply.github.com>
2024-10-22 13:44:57 +0530

    remove

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -35,13 +35,6 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
   
-   @Test
-  public void intentionally_faild() {
-    String someone = "World";
-
-    assertTrue(true);
-  }
-
     @Test
   public void greetShouldIncludeGreetingMessage() {
     String someone = "World, Thanks you making me Happy";
